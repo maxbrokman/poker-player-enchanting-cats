4d3ee5e999cf337b30dc384f0722c006461961cb
Max Brokman <max.brokman@gmail.com>
2024-04-03 10:45:02 +0100

    Hell yeah brother

--- player.py
+++ player.py
@@ -1,9 +1,13 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "Default Python folding player (special version)"
 
     def betRequest(self, game_state):
-        return 0
+        my_index = game_state["in_action"]
+        my_player = game_state["players"][my_index]
+        my_stack = my_player["stack"]
+
+        return my_stack
 
     def showdown(self, game_state):
         pass
