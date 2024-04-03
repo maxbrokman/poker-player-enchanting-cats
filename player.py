c9ca92aa7fc6e36183b9d584aec6347a344c7651
Max Brokman <max.brokman@gmail.com>
2024-04-03 12:53:12 +0100

    add gameround

--- player.py
+++ player.py
@@ -9,9 +9,17 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-from enum import Enum
+from enum import Enum, StrEnum
 from typing import List
 from pydantic import BaseModel
+
+
+class GameRound(StrEnum):
+    PREFLOP = 'preflop'
+    FLOP = 'flop'
+    TURN = 'turn'
+    RIVER = 'river'
+
 
 class PlayerPosition(Enum):
     BUTTON = "button"
@@ -19,6 +27,7 @@ class PlayerPosition(Enum):
     SMALL_BLIND = "small_blind"
     UNDER_THE_GUN = "under_the_gun"
     OTHER = "other"
+
 
 class PlayerModel(BaseModel):
     name: str
@@ -28,6 +37,7 @@ class PlayerModel(BaseModel):
     hole_cards: List[Card]
     version: str
     id: int
+
 
 class GameState(BaseModel):
     players: List[PlayerModel]
@@ -42,13 +52,15 @@ class GameState(BaseModel):
     current_buy_in: int
     pot: int
 
+
 class Card(BaseModel):
-    rank: str # 2-10, J, Q, K, A
-    suit: str # hearts, diamonds, clubs, spades
+    rank: str  # 2-10, J, Q, K, A
+    suit: str  # hearts, diamonds, clubs, spades
 
     def converted_rank(self) -> str:
         """return T if 10 else return rank"""
         return self.rank if self.rank != "10" else "T"
+
 
 class Player:
     VERSION = "Default Python folding player (special version v2)"
@@ -92,59 +104,60 @@ class Hand:
 
         return repr
 
+
 def is_top_twenty_percent_range(a: Card, b: Card) -> bool:
     """Returns True if the two cards are in the top 20% of hands."""
 
     top_twenty_percent_hands = [
-            "AAo", 
-            "KKo", 
-            "QQo", 
-            "JJo", 
-            "TTo", 
-            "99o",
-            "88o",
-            "77o",
-            "66o",
-            "55o",
-            "44o",
+        "AAo",
+        "KKo",
+        "QQo",
+        "JJo",
+        "TTo",
+        "99o",
+        "88o",
+        "77o",
+        "66o",
+        "55o",
+        "44o",
 
-            "AKs",
-            "AQs",
-            "AJs",
-            "ATs",
-            "ATs",
-            "A9s",
-            "A8s",
-            "A7s",
-            "A6s",
-            "A5s",
-            "A4s",
-            "A3s",
-            "A2s",
+        "AKs",
+        "AQs",
+        "AJs",
+        "ATs",
+        "ATs",
+        "A9s",
+        "A8s",
+        "A7s",
+        "A6s",
+        "A5s",
+        "A4s",
+        "A3s",
+        "A2s",
 
-            "AKo",
-            "AQo",
-            "AJo",
-            "ATo",
+        "AKo",
+        "AQo",
+        "AJo",
+        "ATo",
 
-            "KQo",
-            "KJo",
-            "KQs",
-            "KJs",
-            "KTs",
-            "K9s",
+        "KQo",
+        "KJo",
+        "KQs",
+        "KJs",
+        "KTs",
+        "K9s",
 
-            "QJs",
-            "QTs",
-            "Q9s",
+        "QJs",
+        "QTs",
+        "Q9s",
 
-            "JTs",
-            "J9s",
-            "T9s",
-            "98s",
-            "87s",
-            "76s",
-            "65s",
+        "JTs",
+        "J9s",
+        "T9s",
+        "98s",
+        "87s",
+        "76s",
+        "65s",
     ]
 
     return str(Hand([a, b])) in top_twenty_percent_hands
@@ -152,12 +165,10 @@ def is_top_twenty_percent_range(a: Card, b: Card) -> bool:
 
 if __name__ == '__main__':
     # Assert "10" is converted to "T"
-    assert(Card(rank="10", suit="hearts").converted_rank() == "T")
+    assert (Card(rank="10", suit="hearts").converted_rank() == "T")
 
     # Assert Hand representation
-    assert(str(Hand([Card(rank="10", suit="hearts"), Card(rank="K", suit="hearts")])) == "KTs")
+    assert (str(Hand([Card(rank="10", suit="hearts"), Card(rank="K", suit="hearts")])) == "KTs")
 
     # Assert top 20% hands
-    assert(is_top_twenty_percent_range(Card(rank="A", suit="hearts"), Card(rank="K", suit="hearts")))
-
-
+    assert (is_top_twenty_percent_range(Card(rank="A", suit="hearts"), Card(rank="K", suit="hearts")))
